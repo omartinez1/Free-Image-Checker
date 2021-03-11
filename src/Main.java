0a923ce7a4d8938ae6b1a39890f16775bdbd2d85
omartinez1 <67213418+omartinez1@users.noreply.github.com>
2021-03-11 16:12:44 -0500

    Documentation

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,11 @@ import org.jsoup.select.Elements;
 public class Main {
     public static void main(String[] args) {
         try {
+            // connects to website, then get() fetches and parses html file
             Document doc = Jsoup.connect("https://unsplash.com/").get();
+            // we use this to grab each element tag
             Elements imageElements = doc.getElementsByTag("img");
+            // for each element tag we print out the link to the source image
             for (Element element : imageElements) {
                 System.out.println("src: "+element.attr("src"));
             }
