857e73603a27c77a0c79082fd473715027c60609
omartinez1 <67213418+omartinez1@users.noreply.github.com>
2021-03-11 16:07:21 -0500

    Able to grab image from UnSplash

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        try {
+            Document doc = Jsoup.connect("https://unsplash.com/").get();
+            Elements imageElements = doc.getElementsByTag("img");
+            for (Element element : imageElements) {
+                System.out.println("src: "+element.attr("src"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
